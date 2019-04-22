1bd58766524965c312b203995ad0fb7dceadea5b
张甫麟 <960597906@qq.com>
2019-04-22 13:28:33 +0800

    解决由于同事在根目录添加静态文件夹导致build后打开白屏问题

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//   baseUrl: './',
-//   productionSourceMap: false,
-//   devServer: {
-//     proxy: {
-//       '/api': {
-//         // target: 'http://172.16.106.35:9527',
-//         target: 'http://47.92.64.56:9527',
-//         // target: 'http://kntumz.natappfree.cc',
-//         changeOrigin: true,
-//         ws: true,
-//         pathRewrite: {
-//           '^/api': ''
-//         }
-//       }
-//     }
-//   }
-// }
+module.exports = {
+  baseUrl: './',
+  // productionSourceMap: false,
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       // target: 'http://172.16.106.35:9527',
+  //       target: 'http://47.92.64.56:9527',
+  //       // target: 'http://kntumz.natappfree.cc',
+  //       changeOrigin: true,
+  //       ws: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  // }
+}
